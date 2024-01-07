458e5c0d3f1b2dad02051184a7be33d4ff431352
fabriciodiasofficial <142854546+fabriciodiasofficial@users.noreply.github.com>
2024-01-07 11:40:10 -0300

    Add files via upload

---
+++ config/Database.php
@@ -0,0 +1,23 @@
+<!-- config/Database.php -->
+<?php
+class Database {
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $dbname = "mvc";
+    private $conn;
+
+    public function __construct() {
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->dbname}", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Erro na conexão com o banco de dados: " . $e->getMessage());
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
+?>
